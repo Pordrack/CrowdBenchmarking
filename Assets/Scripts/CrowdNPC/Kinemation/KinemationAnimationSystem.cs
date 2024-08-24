0a3adb94c4d144343e35786d126a1ebc34363390
Jérémy <pordrack@gmail.com>
2024-08-24 15:58:28 +0200

    [feat] Can now play a clip on an entity

---
+++ Assets/Scripts/CrowdNPC/Kinemation/KinemationAnimationSystem.cs
@@ -0,0 +1,37 @@
+﻿using Latios.Kinemation;
+using Latios.Transforms;
+using Latios.Transforms.Systems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Burst;
+using Unity.Entities;
+
+using static Unity.Entities.SystemAPI;
+
+namespace CrowdNPC.Kinemation
+{
+    [UpdateBefore(typeof(TransformSuperSystem))]
+    public partial struct KinemationAnimationSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            float t = (float)Time.ElapsedTime;
+
+            foreach ((var bones, var singleClip) in Query<DynamicBuffer<BoneReference>, RefRO<SingleClip>>())
+            {
+                ref var clip = ref singleClip.ValueRO.blob.Value.clips[0];
+                var clipTime = clip.LoopToClipTime(t);
+                for (int i = 1; i < bones.Length; i++)
+                {
+                    var boneSampledLocalTransform = clip.SampleBone(i, clipTime);
+                    var boneTransformAspect = GetAspect<TransformAspect>(bones[i].bone);
+                    boneTransformAspect.localTransformQvvs = boneSampledLocalTransform;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CrowdNPC/Kinemation/SingleClipAuthoring.cs
@@ -0,0 +1,49 @@
+﻿using Latios.Authoring;
+using Latios.Kinemation;
+using Latios.Kinemation.Authoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace CrowdNPC.Kinemation
+{
+    [DisallowMultipleComponent]
+    public class SingleClipAuthoring : MonoBehaviour
+    {
+        public AnimationClip clip;
+    }
+
+    public struct SingleClip : IComponentData
+    {
+        public BlobAssetReference<SkeletonClipSetBlob> blob;
+    }
+
+    [TemporaryBakingType]
+    struct SingleClipSmartBakeItem : ISmartBakeItem<SingleClipAuthoring>
+    {
+        SmartBlobberHandle<SkeletonClipSetBlob> blob;
+
+        public bool Bake(SingleClipAuthoring authoring, IBaker baker)
+        {
+            baker.AddComponent<SingleClip>(baker.GetEntity(TransformUsageFlags.Dynamic));
+            var clips = new NativeArray<SkeletonClipConfig>(1, Allocator.Temp);
+            clips[0] = new SkeletonClipConfig { clip = authoring.clip, settings = SkeletonClipCompressionSettings.kDefaultSettings };
+            blob = baker.RequestCreateBlobAsset(baker.GetComponent<Animator>(), clips);
+            return true;
+        }
+
+        public void PostProcessBlobRequests(EntityManager entityManager, Entity entity)
+        {
+            entityManager.SetComponentData(entity, new SingleClip { blob = blob.Resolve(entityManager) });
+        }
+    }
+
+    class SingleClipBaker : SmartBaker<SingleClipAuthoring, SingleClipSmartBakeItem>
+    {
+    }
+}
